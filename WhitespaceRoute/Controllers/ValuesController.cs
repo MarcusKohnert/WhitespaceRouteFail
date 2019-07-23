c8d6a3cfd9dff10e795b9020492fb122d2033e7c
Marcus Kohnert <marcus.dev@live.com>
2019-07-23 10:41:59 +0200

    Whitespace in attribute route string does not throw an error or anything

---
+++ WhitespaceRoute/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhitespaceRoute.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("whitespace ")]
+        public ActionResult<string> Whitespace() => "Hi";
+
+        [HttpGet("nowhitespace")]
+        public ActionResult<string> NoWhitespace() => "Hi";
+    }
+}
